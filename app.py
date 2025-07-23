6cd3e987a2998e2abc3d02f2e42f059628b5321b
rjarupula1729 <jarupularavi1729@gmail.com>
2025-07-23 22:57:59 +0530

    Update app.py
    added images of the plants

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ plants_data = [
         "space": ["balcony", "window", "terrace"],
         "sunlight": ["partial", "full"],
         "water": ["low", "medium"],
+        "image": "https://media.gettyimages.com/id/1359257193/photo/lush-green-aromatic-holy-basil-plant-looking-magnificent-ocimum-tenuiflorum-lamiaceae-family.jpg?s=1024x1024&w=gi&k=20&c=Oi-lKtpWM_QqLP1-8C8BaxUJPohjkMktZjBRKk62EvM=",
         "care": "Water twice a week. Needs sunlight 3-4 hrs/day."
     },
     {
@@ -18,6 +19,7 @@ plants_data = [
         "space": ["balcony", "terrace", "window"],
         "sunlight": ["partial"],
         "water": ["medium"],
+        "image": "https://media.gettyimages.com/id/1264427026/photo/growing-parsley-in-the-garden.jpg?s=1024x1024&w=gi&k=20&c=bAH7eFAQpOQZy5FXcUZiUB6Ifzc3avvv-V4Nk28r5W4=",
         "care": "Grows fast. Harvest leaves regularly."
     },
     {
@@ -25,6 +27,7 @@ plants_data = [
         "space": ["balcony", "window", "terrace"],
         "sunlight": ["partial", "shade"],
         "water": ["medium", "high"],
+        "image": "https://media.gettyimages.com/id/1216984727/photo/fresh-mint-plant-ayurveda-herb-organic.jpg?s=1024x1024&w=gi&k=20&c=n3pRwE6JoR_OuyWajGZhAdFf1PGedmRgGtx2UYrhUh0=",
         "care": "Water regularly. Pinch tops to grow more leaves."
     },
     {
@@ -32,6 +35,7 @@ plants_data = [
         "space": ["terrace", "balcony"],
         "sunlight": ["full"],
         "water": ["high"],
+        "image": "https://media.gettyimages.com/id/171579643/photo/tomato-greenhouse.jpg?s=1024x1024&w=gi&k=20&c=TUr8artCrHjk5XzhuVJJ-USZwG9jljF8wsnEWjtJ4oQ=",
         "care": "Needs daily watering. Support plant with stick."
     },
     {
@@ -39,6 +43,7 @@ plants_data = [
         "space": ["balcony", "window"],
         "sunlight": ["full", "partial"],
         "water": ["low"],
+        "image": "https://media.gettyimages.com/id/182784203/photo/aloe-vera-plant.jpg?s=1024x1024&w=gi&k=20&c=G6kbUDlYoHnKT-tURnC-5cYL6bkJBd5xA6dpOkTpM40=",
         "care": "Very low maintenance. Water weekly."
     },
     {
@@ -46,6 +51,7 @@ plants_data = [
         "space": ["terrace", "balcony"],
         "sunlight": ["partial"],
         "water": ["medium"],
+        "image": "https://media.gettyimages.com/id/169983791/photo/spinach.jpg?s=1024x1024&w=gi&k=20&c=FMh28QJl8meUK-1VKwQGa5RpjsIovUzy6amDZmypPzA=",
         "care": "Harvest in 30 days. Water every 2-3 days."
     },
     {
@@ -53,6 +59,7 @@ plants_data = [
         "space": ["terrace", "balcony"],
         "sunlight": ["full"],
         "water": ["medium"],
+        "image": "https://media.gettyimages.com/id/154276389/photo/red-green-peppers.jpg?s=1024x1024&w=gi&k=20&c=hgbR1owIXqTNoviiA-EwSr3pW4Kap74ya-y-37o5ssU=",
         "care": "Fertilize monthly. Keep in sunlight."
     },
     {
@@ -60,6 +67,7 @@ plants_data = [
         "space": ["terrace"],
         "sunlight": ["full"],
         "water": ["medium", "high"],
+        "image": "https://media.gettyimages.com/id/2161967118/photo/lemongrass.jpg?s=1024x1024&w=gi&k=20&c=ntAwP5Zg0JJ4sCQ-blrd91rXw1qZ3VDHjlEe0Vek0x8=",
         "care": "Harvest leaves. Grows tall, so needs big pot."
     },
     {
@@ -67,6 +75,7 @@ plants_data = [
         "space": ["terrace", "balcony"],
         "sunlight": ["full"],
         "water": ["medium"],
+        "image": "https://media.gettyimages.com/id/1150140818/photo/curry-leaf-plant.jpg?s=1024x1024&w=gi&k=20&c=KFPSY-Dk686ho8NrSBg7yxPvliznuUnyBaavATbQWxU=",
         "care": "Needs sunlight. Prune regularly."
     },
     {
@@ -74,6 +83,7 @@ plants_data = [
         "space": ["balcony", "terrace"],
         "sunlight": ["partial"],
         "water": ["medium", "high"],
+        "image": "https://media.gettyimages.com/id/128112512/photo/pile-of-ginger-root-on-green-placemat.jpg?s=1024x1024&w=gi&k=20&c=Lxbo7S7XuaGmfz0e3ECPDbeBB1_NzTFvuIP8qp9mXIE="
         "care": "Use grow bag. Keep soil moist."
     }
 ]
@@ -96,6 +106,7 @@ if st.button("Suggest Plants 🌱"):
         if matched_plants:
             st.success(f"Found {len(matched_plants)} suitable plants!")
             for plant in matched_plants:
+                st.image(plant["image"], width=300, caption=plant["name"])
                 st.markdown(f"### 🌿 {plant['name']}")
                 st.write(f"**Space:** {', '.join(plant['space'])}")
                 st.write(f"**Sunlight:** {', '.join(plant['sunlight'])}")
